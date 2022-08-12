469e99a6aadda95bd43fd3ecc15f6f24a2a795ca
saimnaveed <43000682+saimnaveed@users.noreply.github.com>
2022-08-12 16:52:18 +0500

    Add files via upload
    Analysing the Covid Data Using SQL:
    
    --These files have SQL queries used by by me to analyse the COVID Dataset to know about the Covid Infection Count, Covid Deaths, Covid Vaccination, Number of doses in different countries, continent during different periods of time.
    
    --These queries give us a result which can be helpful to know about the damage done to each country by Covid, health situation in a country or a region, infection rate, death rate, vaccination rate etc
    
    -- These queries can be helpful for learning Structured Query Language, its different commands and tools.
    
    -- SQLQuery1.sql has queries applied on the dataset CovidDeaths /  owid-covid-data
    -- SQLQuery2.sql has the queries applied on the dataset CovidVaccinations /  owid-covid-data
    --'owid-covid-data' is the main file from which CovidDeaths and CovidVaccinations files have been derived

---
+++ SQLQuery1.sql
@@ -0,0 +1,76 @@
+-- Getting all contents of Covid Vaccination Table
+
+--Select *
+--From dbo.CovidVaccinations
+--order by 3,4
+
+-- Getting all contents of Covid Deaths Table
+
+--Select *
+--From dbo.CovidDeaths
+--order by 3,4
+
+-- Getting desired content in an order from Covid Deaths
+
+--Select location, date, total_cases, new_cases, total_deaths, population
+--From Analysis..CovidDeaths
+--order by 1,2
+
+-- Getting country wise death percentage
+
+--Select location, SUM(CAST(new_cases as int)) as Total_Cases, SUM(CAST(new_deaths as int)) as Total_Deaths,(SUM(CAST(new_deaths as float))/SUM(CAST(new_cases as float)))*100 as Death_Percentage
+--From Analysis..CovidDeaths
+--group by location
+--order by location
+
+-- Getting death rate change with time w.r.t covid cases
+
+--Select location,date, total_cases, total_deaths, (total_deaths/total_cases)*100 as Death_Rate
+--From Analysis..CovidDeaths
+--where location like '%state%'
+--order by 1,2
+
+--Percentage of Covid Cases w.r.t population/ infection Rates
+
+--Select location,date, total_cases, total_deaths, (total_cases/population)*100 as Covid_Patients_Percentage
+--From Analysis..CovidDeaths
+----where location like '%state%'
+--order by 1,2
+
+-- Getting Highest percentage of Covid Infection w.r.t Every Country
+--Select location, population, max(total_cases) as Highest_Cases, (max(total_cases/population))*100 as Highest_Disease_Rate
+--From Analysis..CovidDeaths
+--group by location,population
+--order by Highest_Disease_Rate Desc
+
+--Getting Highest Death Count by country
+--select location, population, max(cast(total_deaths as int)) as Death_Count, (max(cast(total_deaths as int))/max(total_cases))*100 as Death_Rate
+--from Analysis..CovidDeaths
+--where continent is not Null
+--group by location, population
+--order by Death_Count desc;
+
+--select location, max(cast(total_deaths as int)) as Death_Count, (max(cast(total_deaths as int))/max(total_cases))*100 as Death_Rate
+--from Analysis..CovidDeaths
+--where continent is  Null
+--group by location
+--order by Death_Count desc;
+
+--Getting Highest Death Count w.r.t Continent
+--select continent, max(cast(total_deaths as int)) as Death_Count, (max(cast(total_deaths as int))/max(total_cases))*100 as Death_Rate
+--from Analysis..CovidDeaths
+--where continent is  not Null
+--group by continent
+--order by Death_Count desc;
+
+--Statistics per day all over the world
+
+select sum(new_cases) as Covid_Cases,sum(cast(new_deaths as int)) as Death_Count,(sum(cast(new_deaths as int))/sum(new_cases))*100 as Death_Percentage
+from Analysis..CovidDeaths
+where continent is not Null
+
+select date,sum(new_cases) as Covid_Cases,sum(cast(new_deaths as int)) as Death_Count,(sum(cast(new_deaths as int))/sum(new_cases))*100 as Death_Percentage
+from Analysis..CovidDeaths
+where continent is not Null
+group by date
+order by date
---
+++ SQLQuery2.sql
@@ -0,0 +1,68 @@
+--Getting all contents from the CovidVaccination Table
+
+--select *
+--from Analysis..CovidVaccinations
+
+--Joining all contents of Covid Deaths and Covid Vaccination Table
+
+--select * 
+--from Analysis..CovidDeaths dt join Analysis..CovidVaccinations vac
+--on dt.location=vac.location and dt.date=vac.date
+
+--Getting required data from both table using joins
+--select dt.continent,dt.location,dt.date,dt.population,vac.new_vaccinations
+--from Analysis..CovidDeaths dt 
+--join Analysis..CovidVaccinations vac
+--on dt.location=vac.location 
+--and dt.date=vac.date
+--where dt.continent is not Null
+--order by 2,3
+
+--Getting data from both tables(using joins) , partitioned it by location and used CTE(Common Table Expression) for temporary storage and used it for further calculations
+
+--with vacvspop (Continent, Location,Date,Population, New_Vaccinations, Latest_Vaccination_Count)
+--as(
+--select dt.continent,dt.location,dt.date,dt.population,vac.new_vaccinations , SUM(CONVERT(bigint,vac.new_vaccinations)) OVER (PARTITION BY dt.location order by dt.location,dt.date) as Latest_Vaccination_Count
+--from Analysis..CovidDeaths dt 
+--join Analysis..CovidVaccinations vac
+--	on dt.location=vac.location 
+--	and dt.date=vac.date
+--where dt.continent is not Null
+----order by 2,3
+--)
+--Select *,(Latest_Vaccination_Count/	Population)*100 as Vaccination_Percentage
+--from vacvspop
+
+-- Using Temp Tables for for temporary storage
+
+--Drop Table if exists #VaccinatedPercentage
+--create table #VaccinatedPercentage( Continent nvarchar(255), Location nvarchar(255), Date datetime, Population numeric, new_vaccinations numeric, Latest_Vaccination_Count numeric)
+
+--insert into #VaccinatedPercentage
+
+--select dt.continent,dt.location,dt.date,dt.population,vac.new_vaccinations , SUM(CONVERT(bigint,vac.new_vaccinations)) OVER (PARTITION BY dt.location order by dt.location,dt.date) as Latest_Vaccination_Count
+--from Analysis..CovidDeaths dt 
+--join Analysis..CovidVaccinations vac
+--	on dt.location=vac.location 
+--	and dt.date=vac.date
+--where dt.continent is not Null
+
+--Select * ,(Latest_Vaccination_Count/Population)*100 as Vaccinated_Percentage
+--from #VaccinatedPercentage
+
+--Creating Views for visualizations
+
+Create View vaccinations_percentage as
+select dt.continent,dt.location,dt.date,dt.population,vac.new_vaccinations , SUM(CONVERT(bigint,vac.new_vaccinations)) OVER (PARTITION BY dt.location order by dt.location,dt.date) as Latest_Vaccination_Count
+from Analysis..CovidDeaths dt 
+join Analysis..CovidVaccinations vac
+	on dt.location=vac.location 
+	and dt.date=vac.date
+where dt.continent is not Null
+
+--Selecting/Printing the content of the view
+
+select * from [vaccinations_percentage]
+
+--Droping the view
+--Drop View vaccinations_percentage 
